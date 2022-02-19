b196fa0641c4dbf353d9c9b251e1c8205fa36821
Daniel Catalán <daniel.catalan@usach.cl>
2022-02-19 02:54:45 -0400

    avance

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Boucing_Image
 Programa en javascript que simula el rebote infinito de una imagen
+
+# Instrucciones de uso
+Ejecutar el programa en un servidor local (por el contrario la imagen no se va a poder cargar). En mi caso, ocupé la extensión "live server" de VS CODE.
--- main.js
+++ main.js
@@ -1,26 +1,37 @@
-var ball;
+var ball;  // Objeto que va a rebotar
+var nombreImagen = 'cat.png';
+
+// Resolución del canvas
 const xpixels = 1500;
 const ypixels = 690;
+
+// Dimensiones de la imagen
 const imgX = 250;
 const imgY = 200;
+
+// Hitbox de la imagen
 const bordeX = xpixels - imgX;
 const bordeY = ypixels - imgY;
 
-// Cargar imagen
+// Cargar imagen en "img"
 let img;
 function preload(){
-    img = loadImage('cat.png');
+    img = loadImage(nombreImagen);
 }
 
+// Clases y objetos
+// Clase pelota: Representa el objeto que rebotará en pantalla
 class Pelota{
     constructor(){
         this.x = 0;
         this.y = 0;
         this.xspeed = 5;
         this.yspeed = 5;
+        // Tiene velocidad constante
     }
 
     verificarX(){
+        // Verifica si la imagen está dentro de los límites del canvas en el eje x
         if(this.x <= bordeX && this.x >= 0){
             return true;
         }else{
@@ -29,6 +40,7 @@ class Pelota{
     }
 
     verificarY(){
+        // Verifica si la imagen está dentro de los límites del canvas en el eje y
         if(this.y <= bordeY && this.y >= 0){
             return true;
         }else{
@@ -37,6 +49,7 @@ class Pelota{
     }
 
     actualizar(){
+        // Actualiza la posición del objeto que rebota
         if(this.verificarX()){
             this.x = this.x + this.xspeed;
         }else{
@@ -54,30 +67,24 @@ class Pelota{
     }
 
     mostrar(){
-        /*
-        fill(180);
-        circle(this.x, this.y, 50); */
+        // Carga la imagen en pantalla con su posición respectiva
         image(img, this.x, this.y, imgX, imgY);
         
     }
 }
 
 
-// Programa
+// Programa //
 function setup(){
-    createCanvas(xpixels, ypixels);
-    background(0);
-    ball = new Pelota();
-
-    image(img, 0, 0, 250, 200);
+    createCanvas(xpixels, ypixels); // Crear canvas
+    ball = new Pelota();  // Crear objeto
+    image(img, 0, 0, 250, 200);  // Cargar imagen
     
 }
 
 function draw(){
-    background(255);
-    ball.actualizar();
-    ball.mostrar();
-
-
+    background(255);  // Fondo blanco
+    ball.actualizar();  // Actualizo la posición
+    ball.mostrar();  // Dibujo la posición
 }
 
